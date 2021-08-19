beedfa714d0825db2254a6a338b2001cad7e2912
Chris Ayers <clayers@gmail.com>
2021-08-19 07:54:08 -0400

    added script for files

--- Scripts/generatefiles.sh
+++ Scripts/generatefiles.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-count=$1
+count=${1:-5}
+filename=${2:-file}
+commit=${3:-false}
 for (( filenumber = 1; filenumber <= $count ; filenumber++ )); do
-        echo "Some new random text: $RANDOM" >> file$filenumber.txt
-            git add file$filenumber.txt
-            git commit -m"A random change of $RANDOM to file$filenumber.txt"
+        echo "Some new random text: $RANDOM" >> $filename$filenumber.txt
+        if [ "$commit" = true ]; then
+            git add $filename$filenumber.txt
+            git commit -m"A random change of $RANDOM to $filename$filenumber.txt"
+        fi
         done
